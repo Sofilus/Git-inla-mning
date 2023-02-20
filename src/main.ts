725230557bc2441be8e7f4f294c47743a013fb12
Sofilus <sofia00olsson@gmail.com>
2023-02-20 17:15:12 +0100

    ts - made firstpage disappear when start quiz btn are clicked

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
 import './Style/style.css'
+
+const startQuizBtn = document.querySelector('#startbtn') as HTMLButtonElement;
+
+startQuizBtn?.addEventListener('click', hideFirstpage);
+
+function hideFirstpage () {
+  const firstpage = document.querySelector('#firstpage') as HTMLDivElement;
+  firstpage.classList.add('display-none');
+};
