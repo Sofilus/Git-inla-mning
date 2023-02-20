6208c2c336bee1a44d345e6b1d2b9cf91eb7923c
Sofilus <sofia00olsson@gmail.com>
2023-02-20 23:39:44 +0100

    ts - created html structure for question

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
+import { Question } from './models/questions';
 import './Style/style.css'
+
+
 
 const startQuizBtn = document.querySelector('#startbtn') as HTMLButtonElement;
 
@@ -7,4 +10,66 @@ startQuizBtn?.addEventListener('click', hideFirstpage);
 function hideFirstpage () {
   const firstpage = document.querySelector('#firstpage') as HTMLDivElement;
   firstpage.classList.add('display-none');
+  
+  createQuestionHtml(questionList[0]);
+};
+
+/*------- Question page ------------*/
+
+let questionList: Question[] = [
+  {
+    question: "Vilken hundras ser du på bilden?",
+    image: '/Images/border-collie.jpg',
+    buttons: ["Dalmatin", "Border colli", "Golden"],
+    rightAnswer: 1
+  }
+];
+
+function createQuestionHtml (question: Question) {
+
+  let container = document.querySelector('#questions') as HTMLDivElement;
+
+  let questionContainer = createQuestionDivContainer();
+  container?.appendChild(questionContainer);
+
+  let questionText = createQuestionPTag(question.question);
+  questionContainer.appendChild(questionText);
+
+  let questionImg = createQuestionImg(question.image);
+  questionContainer.appendChild(questionImg);
+
+  let answerBtns = createAnswerBtns(question.buttons);
+  for(let i = 0; i < 3; i++ )
+  questionContainer.appendChild(answerBtns[i]);
+}
+
+function createQuestionDivContainer () {
+  let questionContainer = document.createElement("div") as HTMLDivElement;
+  questionContainer.classList.add('question-container');
+  console.log(questionContainer);
+  return questionContainer;
+}
+
+function createQuestionPTag (question: string){
+  let questionText = document.createElement("p") as HTMLParagraphElement;
+  questionText.innerHTML= question
+  return questionText;
+}
+
+function createQuestionImg (image: string){
+  let questionImg = document.createElement("img") as HTMLImageElement;
+  questionImg.src = image
+  return questionImg;
+}
+
+function createAnswerBtns (buttons: string[])  {
+  let btnList: HTMLButtonElement[] = []
+  for (let i = 0; i < 3; i++) {
+   let answerBtn = document.createElement("btn") as HTMLButtonElement;
+   answerBtn.id = i.toString()
+   answerBtn.innerHTML = buttons[i]
+   btnList.push(answerBtn)
+   
+  }
+  return btnList;
 };
